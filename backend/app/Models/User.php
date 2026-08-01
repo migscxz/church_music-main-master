1daba997c3887bd5a341e4a9ee277965c55de467
migscxz <migslabrador@gmail.com>
2026-08-01 13:19:54 +0800

    backend fix

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -8,6 +8,11 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * @property int $id
+ * @property string $role
+ * @property string $name
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
